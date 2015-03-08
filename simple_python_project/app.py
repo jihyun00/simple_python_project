59f045a8229bf0bd014aa9dc6582d75fd3375ebe
jihyun <noblea1117@gmail.com>
2015-03-09 03:56:14 +0900

    divide login required code

--- simple_python_project/app.py
+++ simple_python_project/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, session, a
 
 from .model import User, Tag
 from .db import db
+from .util import login_required
 
 
 config = {
@@ -24,11 +25,9 @@ def get_tag(tag):
 
 
 @app.route('/', methods=['GET'])
+@login_required
 def index():
-    if 'key' in session:
-        return render_template('index.html')
-    else:
-        return redirect(url_for('login'))
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['GET'])
@@ -59,39 +58,40 @@ def login():
 
 @app.route('/logout', methods=['GET'])
 def logout():
+    session.pop('username', None)
     session.pop('key', None)
+
     return redirect(url_for('login'))
 
 
 @app.route('/tags', methods=['GET'])
+@login_required
 def get_tags():
     return render_template('tag.html')
 
 
 @app.route('/tags', methods=['POST'])
+@login_required
 def post_tags():
-    if 'key' in session:
-        tag = request.values.get('tag')
-        username = session['username']
-        new_tag = get_tag(tag)
+    tag = request.values.get('tag')
+    username = session['username']
+    new_tag = get_tag(tag)
 
-        if tag is None:
-            abort(400)
+    if tag is None:
+        abort(400)
 
-        if new_tag is None:
-            new_tag = Tag(tag, username)
-            db.session.add(new_tag)
-            db.session.commit()
+    if new_tag is None:
+        new_tag = Tag(tag, username)
+        db.session.add(new_tag)
+        db.session.commit()
 
-        else:
-            new_tag.count += 1
-            db.session.commit()
-
-        #username 다를 때는 하나 더 만들기
-
-        return redirect(url_for('get_tags'))
     else:
-        return redirect(url_for('login'))
+        new_tag.count += 1
+        db.session.commit()
+
+    #username 다를 때는 하나 더 만들기
+
+    return redirect(url_for('get_tags'))
 
 
 @app.route('/users/<name>/statistics', methods=['GET'])
---
+++ simple_python_project/util.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from flask import redirect, url_for, session
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'key' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('get_login'))
+        return f(*args, **kwargs)
+    return decorated_function
